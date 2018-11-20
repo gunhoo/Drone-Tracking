8f64bb7cf5670faa463ed76addca2400bc2f6980
gunhoo <gunhoo2016@naver.com>
2018-11-20 13:18:06 +0900

    fix overflow

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ while True:
         frames = []
         # recording
         for i in range(0, int(RATE/CHUNK*RECORD_SECONDS)):
-            data = stream.read(CHUNK)
+            data = stream.read(CHUNK, exception_on_overflow=False)
             frames.append(data)
         # save stream data
         saver(frames, wave, p)
