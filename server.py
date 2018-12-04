86c2f3132f4f2b01f9821c6e4b9a8e7b369632bc
gunhoo <gunhoo2016@naver.com>
2018-12-04 12:18:48 +0900

    Upload server.py

---
+++ server.py
@@ -0,0 +1,68 @@
+import threading
+from socket import *
+import sys
+import datetime
+
+if len(sys.argv) < 2:
+    print("compile error: please input total node number")
+    exit(1)
+totalNodeNum = int(sys.argv[1])
+info = []
+posX = []
+posY = []
+for i in range(0,totalNodeNum+1):
+    info.append(-1)
+    posX.append(-1)
+    posY.append(-1)
+
+class ClientThread(threading.Thread):
+    def __init__(self, clientAddress, connectionSocket):
+        threading.Thread.__init__(self)
+        self.csocket = connectionSocket
+        self.caddr = clientAddress
+    def cal():
+        global info, totalNodeNum
+        predX = 0
+        predY = 0
+        tmp = 0
+        for i in range(0, totalNodeNum+1):
+            predX += (info[i]*posX[i])
+            predY += (info[i]*posY[i])
+            tmp += info[i]
+        predX = predX / tmp
+        predY = predY / tmp
+        now = datetime.now()
+        time = now.strftime('%H:%M:%S')
+        print(time,": Drone's location: (", predX, ",", predY, ")", "accuracy: ", tmp/totalNodeNum)
+    def run(self):
+        global info, posX, posY
+        print("Client Address", self.caddr[0], "connected.")
+        while True:
+            try:
+                message = self.csocket.recv(2048),decode()
+                modifiedMessage = message.split(':')
+                nodeNum = int(modifiedMessage[0])
+                percentage = int(modifiedMessage[1])
+                posX[nodeNum] = int(modifiedMessage[2])
+                posY[nodeNum] = int(modifiedMessage[3])
+                info[nodeNum] = percentage
+                cal()
+            except KeyboardInterrupt:
+                self.csocket.close()
+
+PORT = 21535
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', PORT))
+
+print("The server is ready to receive on port", PORT)
+
+while True:
+    try:
+        serverSocket.listen(1)
+        (connectionSocket, clientAddress) = serverSocket.accept()
+        newThread = ClientThread(clientAddress, connectionSocket)
+        newThread.start()
+    except KeyboardInterrupt:
+        connectionSocket.close()
+        print('Bye bye~')
+        exit()
